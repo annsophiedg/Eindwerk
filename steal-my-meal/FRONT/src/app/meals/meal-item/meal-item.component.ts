531a23fb29a824e6682f1dd2c6a242a547c0d557
Davy Parys <davyparys45@gmail.com>
2019-05-13 15:18:55 +0200

    put meal-item in meals folder

--- steal-my-meal/FRONT/src/app/meals/meal-item/meal-item.component.ts
+++ steal-my-meal/FRONT/src/app/meals/meal-item/meal-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Meal } from '../../models/meal';
+import { Meal } from '../../../models/meal';
 
 @Component({
   selector: 'app-meal-item',
