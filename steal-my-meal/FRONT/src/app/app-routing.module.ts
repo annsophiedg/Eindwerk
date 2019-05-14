f3f27fd12ddf94449e826378aea8c9e11d4fe918
ann-sophie <ann-sophie.dg@hotmail.com>
2019-05-14 23:40:47 +0200

    edit-user: save input values in front

--- steal-my-meal/FRONT/src/app/app-routing.module.ts
+++ steal-my-meal/FRONT/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
   { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
   { path: 'map', loadChildren: './map/map.module#MapPageModule' },
   { path: 'add-meal', loadChildren: './add-meal/add-meal.module#AddMealPageModule' },
-  { path: 'meals', loadChildren: './meals/meals.module#MealsPageModule' }
+  { path: 'meals', loadChildren: './meals/meals.module#MealsPageModule' },
+  { path: 'edit-user', loadChildren: './edit-user/edit-user.module#EditUserPageModule' }
 ];
 @NgModule({
   imports: [
