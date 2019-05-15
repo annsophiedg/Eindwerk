8967a55522ab9d5191444513aac8b5121d8c06b6
Davy Parys <davyparys45@gmail.com>
2019-05-15 18:47:46 +0200

    make first form in add-meal model and dissmis model when clicking x and ready for posting data

--- steal-my-meal/FRONT/src/app/meals/add-meal/add-meal.page.ts
+++ steal-my-meal/FRONT/src/app/meals/add-meal/add-meal.page.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { NavParams } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-meal',
@@ -9,9 +10,20 @@ import { NavParams } from '@ionic/angular';
 export class AddMealPage {
   @Input() value: number;
 
-  constructor(navParams: NavParams) { }
+  constructor(navParams: NavParams, public modal: ModalController) { }
 
   ngOnInit() {
+    
   }
 
+  hideModal(){
+    this.modal.dismiss()
+  }
+
+  postMeal(){
+    this.modal.dismiss({
+      //some values to pass for sending to database
+      // 'result': value
+    })
+  }
 }
