3ce1e69ffac87817d0d9c685ebfd4061abbeaf47
Davy Parys <davyparys45@gmail.com>
2019-06-11 16:33:42 +0200

    set select in price and portions

--- steal-my-meal/FRONT/src/app/meals/add-meal/add-meal.page.ts
+++ steal-my-meal/FRONT/src/app/meals/add-meal/add-meal.page.ts
@@ -43,6 +43,8 @@ export class AddMealPage {
   };
   // end of datepicker modal
 
+  prices = [0.5,1,2,3,4,5,6,7,8,9,10];
+  portions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
   usrID: string ="";
   types = [];
   mealFormObj;
@@ -63,7 +65,6 @@ export class AddMealPage {
 
   ngOnInit() {
     this.usrID = this.userController.getUserId();
-    console.log(this.usrID);
     this.meal = this.formBuilder.group({
       type: ['', Validators.required,],
       name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
